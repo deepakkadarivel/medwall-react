847574873c500120e1632de04e78834507726e4e
Deepakkv <deepakkv@in-deepakkv.local>
2017-09-15 18:53:36 +0530

    [Deepak] Graphql implementation added

--- source/js/client.js
+++ source/js/client.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 import {Provider} from 'react-redux';
 import 'babel-polyfill';
+import {
+  ApolloProvider,
+  createNetworkInterface,
+  ApolloClient,
+} from 'react-apollo';
 
-import configureStore from 'config/store';
 import Client from 'views/Client';
 
 import es6Promise from 'es6-promise';
@@ -13,17 +17,31 @@ import 'isomorphic-fetch';
 // Load SCSS
 import '../scss/app.scss';
 
-es6Promise.polyfill();
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:8080/graphql',
+});
 
-const store = configureStore();
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};
+    }
+    req.options.headers['access_token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImIyMTc2YTUwLTlhMTEtMTFlNy1hNGFlLWVkNDc2ZTQzYWY2YiIsImlhdCI6MTUwNTQ4MDU1MSwiZXhwIjoxNTA1NDgyMzUxfQ.WsqkJI1QItdlHll1z9O6oVJCQjFJZKTfMawxpRSH6Lk';
+    next();
+  },
+}]);
+
+const client = new ApolloClient({networkInterface});
+
+es6Promise.polyfill();
 
 const render = Component => {
   ReactDOM.render(
-    <AppContainer>
-      <Provider store={store}>
+    <ApolloProvider client={client}>
+      <AppContainer>
         <Component/>
-      </Provider>
-    </AppContainer>,
+      </AppContainer>
+    </ApolloProvider>,
     document.getElementById('root')
   );
 };
--- source/js/views/Home/index.js
+++ source/js/views/Home/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {
+  gql,
+  graphql,
+} from 'react-apollo';
 
-const Home = () => {
-  return (
-    <div/>
-  );
-};
+class Home extends Component {
+  render() {
+    console.log('result :', this.props.allRecordsQuery);
+    // 1
+    if (this.props.allRecordsQuery && this.props.allRecordsQuery.loading) {
+      return <div>Loading</div>;
+    }
+
+    // 2
+    if (this.props.allRecordsQuery && this.props.allRecordsQuery.error) {
+      return <div>Error</div>;
+    }
+
+    // 3
+    const recordsToRender = this.props.allRecordsQuery.allRecords.records;
+
+    return (
+      <div>
+        {recordsToRender.map(record => (
+          <li key={record.id}>{record.url}</li>
+        ))}
+      </div>
+    );
+  }
+}
 
 Home.propTypes = {};
 
-export default Home;
+const ALL_RECORDS_QUERY = gql`
+  query AllRecordsQuery {
+    allRecords(skip:0, limit:10) {
+      status
+      message
+      records {
+        id
+        url
+        description
+      }
+    }
+  }
+`;
+
+export default graphql(ALL_RECORDS_QUERY, {name: 'allRecordsQuery'})(Home);
